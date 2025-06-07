6715ed977316ec8ba6bb53f2c3451e0756a64393
Ginny Nguyen <tieuphung2205@gmail.com>
2025-06-07 23:36:07 +0800

    Update Filter.js

--- JS-function/Filter.js
+++ JS-function/Filter.js
@@ -14,3 +14,13 @@ Array.prototype.myFilter = function (callbackFn, thisArg) {
     }
     return result;
 };
+/**
++ Filter() nhận tham số là một callbackFn -> callbackFn sẽ được execute cho từng phần tử của mảng, callbackFn trả về giá trị truthy/falsy
++ Filter() trả về một mảng chứa các phần tử thoả điều kiện của callbackFn
++ Filter() ko làm thay đổi mảng gốc, nó tạo ra một bản copy mới 
+
+Lack-knowledge: 
++ call(): hàm này nhận vào tham số đầu tiên là thisArg, đây sẽ là giá trị biến this trong hàm này hàm apply() cũng tương tự call(), khác cái là apply() nhận vào tham số thứ 2 là một mảng, còn call() 2nd argument dưới dạng liệt kê.
+call(this, arg1, arg2, arg3…) or call(this, …args)
+apply(this, arrs[])
+**/
