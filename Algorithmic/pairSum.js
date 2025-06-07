a4191e8248ffede0da1c0942be2ee6de1b91b01e
Ginny Nguyen <tieuphung2205@gmail.com>
2025-06-07 23:57:17 +0800

    Create pairSum.js

---
+++ Algorithmic/pairSum.js
@@ -0,0 +1,21 @@
+// https://www.greatfrontend.com/questions/algo/pair-sum
+
+/**
+ * @param {number[]} numbers
+ * @param {number} target
+ * @return {number[]}
+ */
+export default function pairSum(numbers, target) {
+  const map = new Map();
+
+    for (let i = 0; i < numbers.length; i++) {
+        map.set(numbers[i], i);
+    }
+    for (let i = 0; i < numbers.length; i++) {
+        const num = target - numbers[i];
+        if (map.has(num) && map.get(num) !== i) {
+            return [i, map.get(num)];
+        }
+    }
+    return []
+}
