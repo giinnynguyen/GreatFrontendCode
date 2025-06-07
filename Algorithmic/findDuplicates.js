eaec03489af2b381012421d488814054cda7d32d
Ginny Nguyen <tieuphung2205@gmail.com>
2025-06-07 13:10:03 +0800

    Update findDuplicates.js

--- Algorithmic/findDuplicates.js
+++ Algorithmic/findDuplicates.js
@@ -1,6 +1,6 @@
 // https://www.greatfrontend.com/questions/algo/array-find-duplicate
 /**
- * Using Set to check if a number already exist, O(n)
+ * Using Set to check if a number already exist, Time complexity: O(n), Space complexity: O(n)
  */
 export default function findDuplicates(numbers) {
   const set = new Set();
@@ -9,4 +9,17 @@ export default function findDuplicates(numbers) {
         set.add(number);
     }
     return false;
+}
+
+/**
+Sort the array first, then check each pair of element in array is duplicated. Time complexity: O(n log n), Space complexity: O(1)
+*/
+export default function findDuplicates2(numbers: number[]): boolean {
+  const n = numbers.sort();
+  for (let i = 0; i < numbers.length; i++) {
+    const num = n[i];
+    const numNext = n[i+1];
+    if (num === numNext) return true;
+  }
+  return false;
 }
