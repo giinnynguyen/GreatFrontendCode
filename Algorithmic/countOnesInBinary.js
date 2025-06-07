102380a332036050f481e660bf2b9bfe22127dde
Ginny Nguyen <tieuphung2205@gmail.com>
2025-06-08 00:41:08 +0800

    Create countOnesInBinary.js

---
+++ Algorithmic/countOnesInBinary.js
@@ -0,0 +1,14 @@
+// https://www.greatfrontend.com/questions/algo/count-ones-in-binary
+
+/**
+ * Step 1: convert num to binary string
+ * Step 2: count 1 in binary string
+ */
+export default function countOnesInBinary(num) {
+  const bi = num.toString(2);
+    let count = 0;
+    for (let s of bi) {
+        if (s === '1') count++;
+    }
+    return count;
+}
