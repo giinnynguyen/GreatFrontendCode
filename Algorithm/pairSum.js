8089fc0dbb5a1728c03dfd8ae4b96555097ab5e6
Ginny Nguyen <tieuphung2205@gmail.com>
2025-07-25 17:19:32 +0700

    Update pairSum.js

--- Algorithm/pairSum.js
+++ Algorithm/pairSum.js
@@ -7,12 +7,12 @@ its pair number is stored in a map
 export default function pairSum(numbers, target) {
   const map = new Map();
 
-    for (let i = 0; i < numbers.length; i++) {
-        const num = target - numbers[i];
-        if (map.has(num) && map.get(num) !== i) {
-            return [map.get(num), i];
-        }
-        map.set(numbers[i], i);
-    }
-    return []
+  for (let i = 0; i < numbers.length; i++) {
+      const num = target - numbers[i];
+      if (map.has(num) && map.get(num) !== i) {
+          return [map.get(num), i];
+      }
+      map.set(numbers[i], i);
+  }
+  return []
 }
